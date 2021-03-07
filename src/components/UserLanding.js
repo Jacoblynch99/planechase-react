27e0baf6088d2f1b813dea27e3213f24e1494949
Jacoblynch99 <jacoblynch99@gmail.com>
2021-03-07 16:52:36 -0600

    css styling on cards

--- src/components/UserLanding.js
+++ src/components/UserLanding.js
@@ -14,18 +14,28 @@ import {
     DialogActions,
     Container,
 } from '@material-ui/core'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const UserLanding = (props) => {
+    const [unvisitedPlanes, setUnvisitedPlanes] = useState([])
+
+    useEffect(() => {
+        props.getPlanes()
+    }, [])
+
+    useEffect(() => {
+        setUnvisitedPlanes(props.cards)
+    }, [])
+
     const consol = () => {
-        console.log(props.cards)
+        console.log(unvisitedPlanes)
     }
 
     return (
         <div style={{ height: '100vh', width: '100vw' }}>
-            <button onClick={() => props.getPlanes()}>fetch</button>
+            <button onClick={consol}>console</button>
 
-            {props.cards.map((item) => {
+            {unvisitedPlanes.map((item) => {
                 return (
                     <div key={item.name + item.originalText}>
                         <img
